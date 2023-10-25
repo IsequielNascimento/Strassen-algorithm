c4050e43a5eeb7c08c3cdfd22a8794c8861981c9
IsequielNascimento <isequielnasciento@gmail.com>
2023-10-24 22:52:39 -0300

    feat: finalização da lógica do algoritmo de Strassen.

--- main.c
+++ main.c
@@ -1,30 +1,40 @@
 #include <stdio.h>
+#define N 2
 
-#define N = 5
+void strassen_multiply(int A[N][N], int B[N][N], int C[N][N]) {
+    int M1, M2, M3, M4, M5, M6, M7;
 
-void multiplicacao_matriz(int A[N][N], int B[N][N], int C[N][N]) {
-    int i, j, k;
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < N; j++) {
-            C[i][j] = 0;
-            for (k = 0; k < N; k++) {
-                // C[i][j] = A[i][0] * B[0][j] + A[i][1] * B[1][j] + ... + A[i][N-1] * B[N-1][j]
-                C[i][j] += A[i][k] * B[k][j];
-            }
+    M1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
+    M2 = (A[1][0] + A[1][1]) * B[0][0];
+    M3 = A[0][0] * (B[0][1] - B[1][1]);
+    M4 = A[1][1] * (B[1][0] - B[0][0]);
+    M5 = (A[0][0] + A[0][1]) * B[1][1];
+    M6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
+    M7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
+
+    C[0][0] = M1 + M4 - M5 + M7;
+    C[0][1] = M3 + M5;
+    C[1][0] = M2 + M4;
+    C[1][1] = M1 - M2 + M3 + M6;
+}
+
+int main() {
+    int A[N][N] = {{1, 2},
+                   {5,6}};
+    int B[N][N] = {{3, 4},
+                 {7,8}};
+    int C[N][N];
+
+    strassen_multiply(A, B, C);
+
+    printf("Resultado da multiplicacao:\n");
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%d ", C[i][j]);
         }
-    }
-}
+        printf("\n");
+    };
+    //system("pause");
 
-void multiplicaca_strassen(int A[N][N], int B[N][N], int C[N][N]) {
-    if ( M <= 5){
-        multiplicacao_matriz(A, B, C);
-        return;
-}
-
-int A11[N/2][N/2], A12[N/2][N/2], A21[N/2][N/2], A22[N/2][N/2];
-int B11[N/2][N/2], B12[N/2][N/2], B21[N/2][N/2], B22[N/2][N/2];
-int C11[N/2][N/2], C12[N/2][N/2], C21[N/2][N/2], C22[N/2][N/2];
-// P1 = (A11 + A22) * (B11 + B22)
-int P1[N/2][N/2], P2[N/2][N/2], P3[N/2][N/2], P4[N/2][N/2], P5[N/2][N/2], P6[N/2][N/2], P7[N/2][N/2];
-
+    return 0;
 }
