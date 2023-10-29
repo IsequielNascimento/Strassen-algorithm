cef2b98948db6f941b59e5fc27edfe056f921b8a
IsequielNascimento <isequielnasciento@gmail.com>
2023-10-29 14:36:19 -0300

    feat: Updated code to implement the Strassen Algorithm for 40x40 float matrices

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-#define N 2
+#include <stdlib.h>
+#define N 40
 
-void strassen_multiply(int A[N][N], int B[N][N], int C[N][N]) {
-    int M1, M2, M3, M4, M5, M6, M7;
-
-    M1 = (A[0][0] + A[1][1]) * (B[0][0] + B[1][1]);
-    M2 = (A[1][0] + A[1][1]) * B[0][0];
-    M3 = A[0][0] * (B[0][1] - B[1][1]);
-    M4 = A[1][1] * (B[1][0] - B[0][0]);
-    M5 = (A[0][0] + A[0][1]) * B[1][1];
-    M6 = (A[1][0] - A[0][0]) * (B[0][0] + B[0][1]);
-    M7 = (A[0][1] - A[1][1]) * (B[1][0] + B[1][1]);
-
-    C[0][0] = M1 + M4 - M5 + M7;
-    C[0][1] = M3 + M5;
-    C[1][0] = M2 + M4;
-    C[1][1] = M1 - M2 + M3 + M6;
+void strassen_multiply(float A[N][N], float B[N][N], float C[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
 }
 
 int main() {
@@ -23,40 +18,43 @@ int main() {
     entrada = fopen("entrada.txt", "r");
     saida = fopen("saida.txt", "w");
 
-    int numero;
-    int matrizA[2][2], matrizB[2][2], matrizC[2][2];
+    if (entrada == NULL || saida == NULL) {
+        printf("Não foi possível abrir os arquivos.\n");
+        return 1;
+    }
 
-    // Ler os números do arquivo e organizar em matrizes 2x2
-    while (fscanf(entrada, "%d", &numero) != EOF) {
-        matrizA[0][0] = numero;
-        fscanf(entrada, "%d", &numero);
-        matrizA[0][1] = numero;
-        fscanf(entrada, "%d", &numero);
-        matrizA[1][0] = numero;
-        fscanf(entrada, "%d", &numero);
-        matrizA[1][1] = numero;
+    float numero;
+    float matrizA[N][N], matrizB[N][N], matrizC[N][N];
 
-        fscanf(entrada, "%d", &numero);
-        matrizB[0][0] = numero;
-        fscanf(entrada, "%d", &numero);
-        matrizB[0][1] = numero;
-        fscanf(entrada, "%d", &numero);
-        matrizB[1][0] = numero;
-        fscanf(entrada, "%d", &numero);
-        matrizB[1][1] = numero;
-
-        // Aplicar o algoritmo de Strassen
-        strassen_multiply(matrizA, matrizB, matrizC);
-
-        // Exportar a matriz resultante para o arquivo de saída
-         fprintf(saida, "\n Resultado Matriz: \n");
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                fprintf(saida, "%d ", matrizC[i][j]);
+    // Ler os números do arquivo e organizar em matrizes 40x40
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (fscanf(entrada, "%f", &numero) == EOF) {
+                break;
             }
-            fprintf(saida, "\n");
+            matrizA[i][j] = numero;
         }
-       
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (fscanf(entrada, "%f", &numero) == EOF) {
+                break;
+            }
+            matrizB[i][j] = numero;
+        }
+    }
+
+    // Aplicar o algoritmo de multiplicação de matrizes
+    strassen_multiply(matrizA, matrizB, matrizC);
+
+    // Exportar a matriz resultante para o arquivo de saída
+    fprintf(saida, "TESTE AAAAAAAAAAAAAAA \n \n", N);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            fprintf(saida, "%.2f ", matrizC[i][j]);
+        }
+        fprintf(saida, "\n");
     }
 
     fclose(entrada);
